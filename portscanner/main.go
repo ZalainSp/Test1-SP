d91a930ec9bf8495d044d1322336c6ca132f283f
Zalain Spain <2023159874@ub.edu.bz>
2025-04-04 03:05:11 +0000

    added start port and end port flag and comments

--- portscanner/main.go
+++ portscanner/main.go
@@ -12,25 +12,25 @@ import (
 	"time"
 )
 
-
+//worker function 
 func worker(wg *sync.WaitGroup, tasks chan string, dialer net.Dialer) {
 	defer wg.Done()
-	maxRetries := 3
+	maxRetries := 3 //set the max amount of retries for connection attemps
     for addr := range tasks {
 		var success bool
 		for i := range maxRetries {      
-		conn, err := dialer.Dial("tcp", addr)
-		if err == nil {
+		conn, err := dialer.Dial("tcp", addr) //attempt tcp connection
+		if err == nil { // if statement if the connection is successful
 			conn.Close()
 			fmt.Printf("Connection to %s was successful\n", addr)
 			success = true
-			break
+			break //exit loop
 		}
-		backoff := time.Duration(1<<i) * time.Second
+		backoff := time.Duration(1<<i) * time.Second //if there is no connection calculate total backoff and retry
 		fmt.Printf("Attempt %d to %s failed. Waiting %v...\n", i+1,  addr, backoff)
-		time.Sleep(backoff)
+		time.Sleep(backoff) //wait before trying again
 	    }
-		if !success {
+		if !success { //if everything fails print error message
 			fmt.Printf("Failed to connect to %s after %d attempts\n", addr, maxRetries)
 		}
 	}
@@ -38,31 +38,32 @@ func worker(wg *sync.WaitGroup, tasks chan string, dialer net.Dialer) {
 
 func main() {
 
-	var wg sync.WaitGroup
-	tasks := make(chan string, 100)
+	var wg sync.WaitGroup //waitgroup to manage go routines
+	tasks := make(chan string, 100) //
 
-	//command line target flag
-    target := flag.String("target","scanme.nmap.org", "specify the IP address or hostname" )
-	flag.Parse()
+	//command line flags
+    target := flag.String("target","scanme.nmap.org", "specify the IP address or hostname" ) //target IP address or hostname
+	startport := flag.Int("start port", 1, "Start port (default: 1) ") //starting port for scanning
+	endport := flag.Int("end port", 1042, "End port(default: 1024)") //ending port for scanning
+	flag.Parse() //parse command line flags
 
-	dialer := net.Dialer {
-		Timeout: 5 * time.Second,
+	dialer := net.Dialer { //handle TCP connections 
+		Timeout: 5 * time.Second, //timout for each connection
 	}
   
-	workers := 100
+	workers := 100 //number of worker go routines
 
     for i := 1; i <= workers; i++ {
-		wg.Add(1)
-		go worker(&wg, tasks, dialer)
+		wg.Add(1) 
+		go worker(&wg, tasks, dialer) //start the worker go routine
 	}
 
-	ports := 512
 
-	for p := 1; p <= ports; p++ {
-		port := strconv.Itoa(p)
-        address := net.JoinHostPort(*target, port)
-		tasks <- address
+	for p := *startport; p <= *endport; p++ {
+		port := strconv.Itoa(p) //convert port number to a string
+        address := net.JoinHostPort(*target, port) //put target and port into a address
+		tasks <- address //send address to worker channel
 	}
-	close(tasks)
-	wg.Wait()
+	close(tasks) //close tasks
+	wg.Wait() //wait for all worker go routines to finish
 }
